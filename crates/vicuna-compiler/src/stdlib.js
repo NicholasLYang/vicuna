67fe64ea0238b4f831ebd756ab933ef592f54cdf
Nicholas Yang <nick@nicholasyang.com>
2024-01-06 02:22:29 -0500

    Adjusted the print function to indent

--- crates/vicuna-compiler/src/stdlib.js
+++ crates/vicuna-compiler/src/stdlib.js
@@ -1,19 +1,24 @@
 function print(...args) {
-  for (const arg of args) {
-    if (Array.isArray(arg)) {
-      console.log("[");
-      for (const elem of arg) {
-        print(elem);
-      }
-      console.log("]");
-    } else if (arg.hasOwnProperty("__type__")) {
-      if (arg.hasOwnProperty("__enum__")) {
-        console.log(`${arg.__enum__}::${arg.__type__}`);
+  function printHelper(indent, ...args) {
+    const indentStr = " ".repeat(indent);
+    for (const arg of args) {
+      if (Array.isArray(arg)) {
+        console.log(`${indentStr}[`);
+        for (const elem of arg) {
+          printHelper(indent + 2, elem);
+        }
+        console.log(`${indentStr}]`);
+      } else if (arg.hasOwnProperty("__type__")) {
+        if (arg.hasOwnProperty("__enum__")) {
+          console.log(`${indentStr}${arg.__enum__}::${arg.__type__}`);
+        } else {
+          console.log(`${indentStr}${arg.__type__}`);
+        }
       } else {
-        console.log(arg.__type__);
+        console.log(`${indentStr}${arg}`);
       }
-    } else {
-      console.log(arg);
     }
   }
+
+  printHelper(0, ...args);
 }
